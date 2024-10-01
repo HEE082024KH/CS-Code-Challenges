643d7a3c3dbe45deb9a557adc632fb645c1616d3
hee082024kh <hee082024@gmail.com>
2024-10-01 10:57:28 +0200

    ChallengeTree

--- Program.cs
+++ Program.cs
@@ -8,51 +8,54 @@ class Program
         Challenge_two two = new Challenge_two();
         Challenge_three three = new Challenge_three();
 
+        // Challenge one
+        one.PrintNumbers();
+        Console.WriteLine();
+
+        string[] challengeArray = [];
+        one.PrintStrings(challengeArray);
+        Console.WriteLine();
+
+        double i = 0;
+        Console.WriteLine("Positive, negative or zero");
+        one.CheckIfNumberIsPositiveOrNegative(i);
+        Console.WriteLine();
+
+        // Challenge two
+        two.GuessNumber();
+        Console.WriteLine();
+
+        two.Fibonacci(5);
+        Console.WriteLine();
+
+        int number = 0;
+        Console.WriteLine("Even or Odd");
+        two.CheckIfNumberIsEvenOrOdd(number);
+        Console.WriteLine();
+
+        // Challenge tree
+        Console.WriteLine("FizzBuzz");
+        three.FizzBuzz();
+        Console.WriteLine();
+
+        Console.WriteLine(three.ReverseString("Tullball"));
 
         // validate some methods
         void ValidateRecursiveFactorial()
         {
-            // int n = 5;
-            // int expected = 120; // 5! = 120
-            // int result = three.RecursiveFactorial(n);
+            Console.WriteLine("Factorial");
+            int n = 5;
+            int expected = 120; // 5! = 120
+            int result = three.RecursiveFactorial(n);
 
-            // if (result == expected)
-            // {
-            //     Console.WriteLine($"Test passed: RecursiveFactorial({n}) = {expected}");
-            // }
-            // else
-            // {
-            //     Console.WriteLine($"Test failed: RecursiveFactorial({n}) = {result}, expected: {expected}");
-            // }
-
-            // Challenge one
-            Challenge_one one = new();
-            one.PrintNumbers();
-            Console.WriteLine();
-
-            string[] challengeArray = [];
-            one.PrintStrings(challengeArray);
-            Console.WriteLine();
-
-            double i = 0;
-            one.CheckIfNumberIsPositiveOrNegative(i);
-            Console.WriteLine();
-
-            // Challenge two
-            Challenge_two two = new();
-            two.GuessNumber();
-            Console.WriteLine();
-
-            two.Fibonacci(5);
-            Console.WriteLine();
-
-            int number = 0;
-            two.CheckIfNumberIsEvenOrOdd(number);
-            Console.WriteLine();
-
-            // Challenge tree
-            three.FizzBuzz();
-
+            if (result == expected)
+            {
+                Console.WriteLine($"Test passed: RecursiveFactorial({n}) = {expected}");
+            }
+            else
+            {
+                Console.WriteLine($"Test failed: RecursiveFactorial({n}) = {result}, expected: {expected}");
+            }
         }
 
         ValidateRecursiveFactorial();
